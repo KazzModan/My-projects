63d63ce431f93ba9a504cfea2fad26bfaf61e3f9
Kostya Klopov <kklopov64@gmail.com>
2020-06-23 12:57:01 +0300

    Changed Plan.cpp, check it for my HM

--- Plan/Plan/main.cpp
+++ Plan/Plan/main.cpp
@@ -1,41 +1,35 @@
-
+#include <Windows.h>
 #include <iostream>
-using namespace std;
 #define CHECK_TEXT ((text[i] >= 'A' && text[i] <= 'Z') || (text[i] >= 'a' && text[i] <= 'z') || (text[i] >= '0' && text[i] <= '9') || text[i] == ':')
+using namespace std;
+
+void ShowMenu();
+void Menu();
 template<typename T>
-class List
+class SList
 {
 public:
-	List();
+	SList();
 
-	//pop front
-	void pop_front();
+	void deleteFromHead();
 
-	//pop back
-	void pop_back();
+	void deleteFromTail();
 
-	//push front
-	void push_front(T data);
+	void addToHead(T data);
 
-	//push back
-	void push_back(T data);
+	void addToTail(T data);
 
-	//clear - delete all elements
 	void clear();
 
-	//get size
-	size_t getSize() { return size; }
+	size_t getSize() { return this->size; }
 
-	//insert element to index
 	void insert(T data, size_t index);
 
-	//remove element at index
 	void remove(size_t index);
 
-	//operator []
 	T& operator[](const size_t index);
 
-	~List();
+	~SList();
 
 private:
 	template<typename T>
@@ -57,36 +51,36 @@ private:
 
 
 template<typename T>
-List<T>::List()
+SList<T>::SList()
 {
-	size = 0;
-	head = nullptr;
+	this->size = 0;
+	this->head = nullptr;
 }
 
 
 template<typename T>
-List<T>::~List()
+SList<T>::~SList()
 {
 	clear();
 }
 
 
 template<typename T>
-void List<T>::pop_front()
+void SList<T>::deleteFromHead()
 {
 	Node<T>* temp = head;
 
-	head = head->next;
+	this->head = this->head->next;
 
 	delete temp;
-	size--;
+	this->size--;
 }
 
 template<typename T>
-void List<T>::push_back(T data)
+void SList<T>::addToTail(T data)
 {
-	if (head == nullptr)
-		head = new Node<T>(data);
+	if (this->head == nullptr)
+		this->head = new Node<T>(data);
 	else
 	{
 		Node<T>* current = this->head;
@@ -96,22 +90,25 @@ void List<T>::push_back(T data)
 		current->next = new Node<T>(data);
 	}
 
-	size++;
+	this->size++;
 }
 
 template<typename T>
-void List<T>::clear()
+void SList<T>::clear()
 {
-	for (size_t i = 0; i < size; i++)
-		pop_front();
+	for (size_t i = 0; i < this->size; i++)
+		deleteFromHead();
 }
 
 
 template<typename T>
-T& List<T>::operator[](const size_t index)
+T& SList<T>::operator[](const size_t index)
 {
 	size_t counter = 0;
 	Node<T>* current = this->head;
+
+	if (index >= this->size)
+		throw "error you cannot access an index that does not innovate\n";
 
 	while (current != nullptr)
 	{
@@ -121,21 +118,23 @@ T& List<T>::operator[](const size_t index)
 		current = current->next;
 		counter++;
 	}
+
+	throw "error you cannot access an index that does not innovate\n";
 }
 
 template<typename T>
-void List<T>::push_front(T data)
+void SList<T>::addToHead(T data)
 {
-	head = new Node<T>(data, head);
-	size++;
+	this->head = new Node<T>(data, this->head);
+	this->size++;
 }
 
 template<typename T>
-void List<T>::insert(T data, size_t index)
+void SList<T>::insert(T data, size_t index)
 {
 
 	if (index == 0)
-		push_front(data);
+		addToHead(data);
 	else
 	{
 		Node<T>* previous = this->head;
@@ -145,15 +144,15 @@ void List<T>::insert(T data, size_t index)
 
 		Node<T>* newNode = new Node<T>(data, previous->next);
 		previous->next = newNode;
-		size++;
+		this->size++;
 	}
 }
 
 template<typename T>
-void List<T>::remove(size_t index)
+void SList<T>::remove(size_t index)
 {
 	if (index == 0)
-		pop_front();
+		deleteFromHead();
 	else
 	{
 		Node<T>* previous = this->head;
@@ -165,105 +164,19 @@ void List<T>::remove(size_t index)
 		previous->next = toDelete->next;
 		delete toDelete;
 
-		size--;
+		this->size--;
 	}
 
 }
 
 template<typename T>
-void List<T>::pop_back()
+void SList<T>::deleteFromTail()
 {
 	remove(size - 1);
 }
-class Planner
-{
-public:
 
-private:
-	List<Day> list;
-};
 
-class Day
-{
-public:
-	Day(const Day& day)
-	{
-		this->date = day.getDate();
-		this->list = day.getList();
-	}
 
-	void addEvent()
-	{
-		Event tempEvent;
-		string temp;
-
-		do
-		{
-			cout << "enter event name: ";
-			cin >> temp;
-
-		} while (!tempEvent.setHeader(temp));
-
-		do
-		{
-			cout << "enter event info: ";
-			 cin>>temp;
-
-		} while (!tempEvent.setInfo(temp));
-
-		do
-		{
-			cout << "this event is important? enter yes or no";
-			cin >> temp;
-
-		} while (temp != "yes" && temp != "no");
-
-		if (tempEvent.setPrior)
-		{
-			list.push_front(tempEvent);
-		}
-		else
-		{
-			this->list.push_back(tempEvent);
-		}
-	}
-	
-
-	void removeEvent()
-	{
-		size_t length = this->list.getSize();
-		size_t index;
-
-		do
-		{
-			cout << "enter event index [0: " << length - 1 << " ]: ";
-			cin >> index;
-		} while (index < 0 || index >= length);
-
-		this->list.remove(index);
-	}
-
-	void setDate()
-	{
-		size_t day;
-		size_t month;
-		size_t year;
-			cout << "enter number of day, month and year : ";
-			cin >> day >> month >> year;
-	}
-	Data getDate() const
-	{
-		return this->date;
-	}
-
-	List<Event> getList() const
-	{
-		return List<Event>(list);
-	}
-private:
-	Data date;
-	List<Event> list;
-};
 class Data
 {
 public:
@@ -300,6 +213,18 @@ public:
 			_year = year;
 		}
 	}
+	void print()
+	{
+		if (_day < 10)
+			cout << "0" << _day;
+		else
+			cout << _day;
+		if (_month < 10)
+			cout << ":0" << _month;
+		else
+			cout << ":" << _month;
+		cout << ":" << _year << endl;
+	}
 	enum LIMITS
 	{
 		MIN_DAY = 1, MIN_MONTH = 1, MIN_YEAR = 2020, MAX_DAY = 31, MAX_MONTH = 12, MAX_YEAR = 2077
@@ -309,6 +234,7 @@ private:
 	size_t _month;
 	size_t _year;
 };
+
 class Event
 {
 public:
@@ -318,6 +244,7 @@ public:
 		_data.setMonth(month);
 		_data.setYear(year);
 	}
+
 	bool setHeader(string text)
 	{
 		size_t length = text.length();
@@ -336,9 +263,9 @@ public:
 	void setPrior()
 	{
 		string temp;
-		cout << "this event is important? Input yes or not: ";
+		cout << "this event is important? Input y or n:\n";
 		cin >> temp;
-		if (temp == "yes")
+		if (temp == "y")
 		{
 			_prior = true;
 		}
@@ -360,12 +287,255 @@ public:
 		}
 		return false;
 	}
+	bool getPrior() const
+	{
+		return _prior;
+	}
+	void print()
+	{
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+		if (this->getPrior())
+		{
+			SetConsoleTextAttribute(hConsole, WORD(4));
+			cout << "name: " << this->_header << endl;
+		}
+		else
+		{
+			SetConsoleTextAttribute(hConsole, WORD(2));
+			cout << "name: " << this->_header << endl;
+		}
+		SetConsoleTextAttribute(hConsole, WORD(15));
+		cout << "info: " << this->_info << endl;
+	}
 private:
 	Data _data;
 	bool _prior;
 	string _header;
 	string _info;
 };
+class Day
+{
+public:
+	Day(const Day& day)
+	{
+		this->date = day.getDate();
+		this->list = day.getList();
+	}
+	Day(Data date, SList<Event>list)
+	{
+		this->setDate(date);
+		this->list = list;
+	}
+	Day()
+	{
+
+	}
+	void addEvent()
+	{
+		Event tempEvent;
+		string temp;
+
+		do
+		{
+			cout << "enter event name: ";
+			cin >> temp;
+			tempEvent.setHeader(temp);
+		} while (!tempEvent.setHeader(temp));
+
+		do
+		{
+			cout << "enter event info: ";
+			cin >> temp;
+			tempEvent.setInfo(temp);
+		} while (!tempEvent.setInfo(temp));
+
+		tempEvent.setPrior();
+		if (tempEvent.getPrior())
+		{
+			list.addToHead(tempEvent);
+		}
+
+		else
+		{
+			list.addToTail(tempEvent);
+		}
+
+	}
+	void print()
+	{
+		this->date.print();
+
+		for (size_t i = 0; i < list.getSize(); i++)
+			list[i].print();
+	}
+
+	void removeEvent()
+	{
+		size_t length = this->list.getSize();
+		size_t index;
+
+		do
+		{
+			cout << "enter event index [0: " << length - 1 << " ]: ";
+			cin >> index;
+		} while (index < 0 || index >= length);
+
+		this->list.remove(index);
+	}
+
+	void setDate()
+	{
+		size_t day;
+		size_t month;
+		size_t year;
+		cout << "enter number of day, month and year : ";
+		cin >> day >> month >> year;
+	}
+	void setDate(Data date)
+	{
+		this->date = date;
+	}
+	Data getDate() const
+	{
+		return this->date;
+	}
+
+	SList<Event> getList() const
+	{
+		return SList<Event>(list);
+	}
+private:
+	Data date;
+	SList<Event> list;
+};
+class Planner
+{
+public:
+	void AddDay()
+	{
+		char choose = 0;
+		SList<Event> list;
+		int day, month, year;
+		Data date;
+		cout << "Enter day\n";
+		cin >> day;
+		date.setDay(day);
+		cout << "Enteer month\n";
+		cin >> month;
+		date.setMonth(month);
+		cout << "Enter year\n";
+		cin >> year;
+		date.setYear(year);
+		Day dayEv;
+		dayEv.setDate(date);
+		this->list.addToHead(dayEv);
+	}
+	void ShowAllDay()
+	{
+		system("cls");
+
+		for (size_t i = 0; i < list.getSize(); i++)
+		{
+			this->list[i].print();
+			cout << endl;
+
+		}
+	}
+	void AddEventToDay()
+	{
+		int index = findDay();
+		if (index == -1)
+			return;
+		list[index].addEvent();
+	}
+	void removeDay()
+	{
+		int index = findDay();
+		if (index == -1)
+			return;
+		list.remove(index);
+	}
+	void removeEventFromDay()
+	{
+		int index = findDay();
+		if (index == -1)
+			return;
+		list[index].removeEvent();
+	}
+	int findDay()
+	{
+		size_t length = this->list.getSize();
+		int index;
+
+		do
+		{
+			cout << "enter day index or -1 to exit [ 0: " << length - 1 << " ]: ";
+			cin >> index;
+
+			if (index == -1)
+				return -1;
+		} while (size_t(index) < 0 || size_t(index) >= length);
+
+		return index;
+	}
+
+
+	Planner()
+	{
+
+	}
+private:
+	SList<Day> list;
+};
 int main()
 {
+	Menu();
+}
+void ShowMenu()
+{
+	cout << "(1) Add day.\n";
+	cout << "(2) Add event to day.\n";
+	cout << "(3) show planner.\n";
+	cout << "(4) remove day.\n";
+	cout << "(5) remove event from day.\n";
+	cout << "(0) exit from menu.\n";
+}
+
+void Menu()
+{
+	char choise;
+	bool isWork = true;
+	Planner planner;
+
+
+	do
+	{
+		ShowMenu();
+		cin >> choise;
+		system("cls");
+		switch (choise)
+		{
+		case '1':
+			planner.AddDay();
+			break;
+		case '2':
+			planner.AddEventToDay();
+			break;
+		case '3':
+			planner.ShowAllDay();
+			break;
+		case '4':
+			planner.removeDay();
+			break;
+		case '5':
+			planner.removeEventFromDay();
+			break;
+		case '0':
+			isWork = false;
+			break;
+		}
+	} while (isWork != false);
+
+
 }
